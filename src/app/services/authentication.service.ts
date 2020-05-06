b6b891d083d2d5e5953751c969f4af5b77f3279f
Nazaré Lisboa <zaza.naza95@gmail.com>
2020-05-06 16:19:01 -0300

    created authentication

---
+++ src/app/services/authentication.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { environment } from "src/environments/environment";
+import { Router } from "@angular/router";
+import { Observable } from "rxjs";
+import { IAuthentication } from "../interfaces/interfaces";
+
+@Injectable({
+  providedIn: "root",
+})
+export class AuthenticationService {
+  constructor(private http: HttpClient, private router: Router) {}
+
+  isLoggedIn(): boolean {
+    return !!localStorage.getItem("token");
+  }
+
+  login(user): Observable<IAuthentication> {
+    return this.http.post<IAuthentication>(
+      `${environment.urlHeroku}/login`,
+      user
+    );
+  }
+
+  logout(): void {
+    this.http.get(`${environment.urlHeroku}/logout`).subscribe(() => {
+      localStorage.clear();
+      this.router.navigate(["/login"]);
+    });
+  }
+}
