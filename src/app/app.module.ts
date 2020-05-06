d964da3938994d5cc8644ca258775ff90ef61018
Nazaré Lisboa <zaza.naza95@gmail.com>
2020-05-05 23:28:54 -0300

    created helpers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,12 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { AppComponent } from "./app.component";
 import { CoreModule } from "./core/core.module";
+import { Toast } from "./shared/helpers/Toast/toast";
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, CoreModule],
-  providers: [],
+  providers: [Toast],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
