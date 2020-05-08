6c690e8ae90b7d118db31c7d16b3587f8265fced
Nazaré Lisboa <zaza.naza95@gmail.com>
2020-05-08 14:16:42 -0300

    html and css test in the seller registration component

--- src/app/core/components/seller-registration/seller-registration.component.spec.ts
+++ src/app/core/components/seller-registration/seller-registration.component.spec.ts
@@ -4,12 +4,12 @@ import { RouterTestingModule } from "@angular/router/testing";
 import { Toast } from "src/app/shared/helpers/toast/toast";
 import { HttpClientModule } from "@angular/common/http";
 import { CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
-import { LoginComponent } from "./login.component";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { SellerRegistrationComponent } from "./seller-registration.component";
 
-describe("LoginComponent", () => {
-  let component: LoginComponent;
-  let fixture: ComponentFixture<LoginComponent>;
+describe("SellerRegistrationComponent", () => {
+  let component: SellerRegistrationComponent;
+  let fixture: ComponentFixture<SellerRegistrationComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -19,12 +19,12 @@ describe("LoginComponent", () => {
         FormsModule,
         ReactiveFormsModule,
       ],
-      declarations: [LoginComponent],
+      declarations: [SellerRegistrationComponent],
       providers: [Toast],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
     });
 
-    fixture = TestBed.createComponent(LoginComponent);
+    fixture = TestBed.createComponent(SellerRegistrationComponent);
     component = fixture.componentInstance;
   }));
 
@@ -37,21 +37,21 @@ describe("LoginComponent", () => {
       fixture.detectChanges();
       let de = fixture.debugElement.query(By.css("h1")).nativeElement;
 
-      expect(de.innerText).toContain("Login");
+      expect(de.innerText).toContain("Cadastro de Vendedor");
     });
 
     it("should be have a title button", () => {
       fixture.detectChanges();
       let de = fixture.debugElement.query(By.css("button")).nativeElement;
 
-      expect(de.innerText).toContain("Entrar");
+      expect(de.innerText).toContain("Cadastrar");
     });
 
     it("should be have a title on tag A", () => {
       fixture.detectChanges();
       let de = fixture.debugElement.query(By.css("a")).nativeElement;
 
-      expect(de.innerText).toContain("Não tem um conta? Cadastre-se");
+      expect(de.innerText).toContain("Já tem uma conta? Faça login");
     });
   });
 
@@ -70,9 +70,9 @@ describe("LoginComponent", () => {
       expect(el).toBeTruthy();
     });
 
-    it("Should be class `no-have-account`", () => {
+    it("Should be class `have-account`", () => {
       fixture.detectChanges();
-      const el = fixture.debugElement.query(By.css(".no-have-account"));
+      const el = fixture.debugElement.query(By.css(".have-account"));
 
       expect(el).toBeTruthy();
     });
