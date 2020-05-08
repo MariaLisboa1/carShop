f4f9f7a6ae3605045dd5aa76677ac2adaa007995
Nazaré Lisboa <zaza.naza95@gmail.com>
2020-05-08 14:09:49 -0300

    html and css test in the login component

--- src/app/core/components/home/home.component.spec.ts
+++ src/app/core/components/home/home.component.spec.ts
@@ -1,9 +1,4 @@
-import {
-  async,
-  ComponentFixture,
-  TestBed,
-  fakeAsync,
-} from "@angular/core/testing";
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 import { By } from "@angular/platform-browser";
 import { RouterTestingModule } from "@angular/router/testing";
 import { HomeComponent } from "./home.component";
